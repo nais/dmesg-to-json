0556f76fdf899e5028c3b4a5466ec5fe8270e8dd
Christian C <x10an14@gmail.com>
2020-09-16 12:38:45 +0200

    feature: Read 50 lines from kernel log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use rmesg::{kernel_log_timestamps_enable, RMesgLinesIterator, SUGGESTED_POLL_INTERVAL};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    kernel_log_timestamps_enable(true)?;
+    let iterator = RMesgLinesIterator::with_options(false, SUGGESTED_POLL_INTERVAL)?;
+    let (max_lines, mut counter) = (50, 0);
+    for line in iterator {
+        if counter >= max_lines {
+            break;
+        }
+        println!("\nCurrent line:\n{}", &line?);
+        counter += 1;
+    }
+    Ok(())
 }
